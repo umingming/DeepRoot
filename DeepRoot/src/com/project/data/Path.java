f6895019d753035c5f925deefb5f8cc1382ca900
umingming <u_0112@naver.com>
2021-12-14 22:52:21 +0900

    Add Path.java

---
+++ DeepRoot/src/com/project/data/Path.java
@@ -0,0 +1,17 @@
+package com.project.data;
+
+public class Path {
+	public static String category = "data\\문제_분류.txt";
+	public static String question = "data\\문제.txt";
+	public static String quotation = "data\\명언.txt";
+	public static String score = "data\\학습량.txt";
+	public static String study = "data\\학습.txt";
+	public static String user = "data\\회원.txt";
+	public static String wrongAnswer = "data\\오답.txt";
+}
+
+
+
+
+
+
